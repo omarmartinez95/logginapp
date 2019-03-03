d5e86a2dd3c9a1195a53481957c7093f4d03ea1a
Omar Martinez <omarmartinez95@hotmail.com>
2019-03-03 09:41:07 -0500

    Se crean los enviroment para la conexion con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+// una vez nuestra aplicacion este en produccion.
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC5TV3MF9_Nc4qYNJw5P2xmgSqYuSU7uCU",
+    authDomain: "logginapp-df220.firebaseapp.com",
+    databaseURL: "https://logginapp-df220.firebaseio.com",
+    projectId: "logginapp-df220",
+    storageBucket: "logginapp-df220.appspot.com",
+    messagingSenderId: "1092226265582"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyC5TV3MF9_Nc4qYNJw5P2xmgSqYuSU7uCU",
+    authDomain: "logginapp-df220.firebaseapp.com",
+    databaseURL: "https://logginapp-df220.firebaseio.com",
+    projectId: "logginapp-df220",
+    storageBucket: "logginapp-df220.appspot.com",
+    messagingSenderId: "1092226265582"
+  }
 };
 
 /*
